f6c4da2e574613530b6de3b4f8bc377c284afea8
Phaedrus <phraznikov@gmail.com>
2019-10-28 16:29:26 -0700

    Fold in GiftedChat

--- App.js
+++ App.js
@@ -1,113 +1,99 @@
-import React, {useState} from 'react';
-import {KeyboardAvoidingView, StyleSheet, Alert, View} from 'react-native';
+import React from 'react';
+import {KeyboardAvoidingView, StyleSheet, Text, View} from 'react-native';
 import Constants from 'expo-constants';
-import {Text, Input, ThemeProvider, Header} from 'react-native-elements';
+import {GiftedChat} from 'react-native-gifted-chat';
 
-export default function App() {
-  const [userMessage, setUserMessage] = useState();
-  const [agentMessage, setAgentMessage] = useState();
+const uuidv4 = require('uuid/v4');
 
-  async function queryAgent(query) {
-    try {
-      setUserMessage(query);
-      const url = 'https://robopeterson-api.herokuapp.com/api/messages';
-      const deviceId = Constants.installationId;
-      const res = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({query: query, deviceId: deviceId}),
+class App extends React.Component {
+  state = {
+    messages: [],
+  };
+
+  componentWillMount() {
+    const deviceId = Constants.installationId;
+    const url = `https://robopeterson-api.herokuapp.com/api/messages/${deviceId}`;
+    fetch(url)
+      .then(res => {
+        console.log('res from getMessages:\n', res);
+        if (res.status === 200) {
+          console.log('res.status === 200');
+          res
+            .json()
+            .then(resJSON => {
+              console.log('resJSON from getMessages:\n', resJSON);
+              this.setState({
+                messages: resJSON,
+              });
+            })
+            .catch(err => {
+              console.error(err);
+            });
+        } else {
+          this.setState({
+            messages: [],
+          });
+        }
+      })
+      .catch(err => {
+        console.error(err);
       });
-      const response = await res.json();
-      const message = response.message;
-      setAgentMessage(message);
-    } catch (err) {
-      console.error(err.message);
-      Alert.alert('Error', 'There was an error querying the agent.', [
-        {text: 'OK'},
-      ]);
-      setAgentMessage(err.message);
-    }
   }
 
-  return (
-    <ThemeProvider>
-      <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
-        <Header
-          centerComponent={{text: 'ROBOPETERSON', style: {color: 'white'}}}
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+    const url = 'https://robopeterson-api.herokuapp.com/api/messages';
+    const deviceId = Constants.installationId;
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({query: messages[0].text, deviceId: deviceId}),
+    })
+      .then(res => {
+        res.json().then(resJSON => {
+          const agentMessages = [
+            {
+              _id: uuidv4(),
+              createdAt: new Date(),
+              text: resJSON.message,
+              image: resJSON.meme ? resJSON.meme.image : '',
+              user: {
+                _id: 'robopeterson-95686',
+                name: 'robopeterson-95686',
+                avatar: 'https://f4.bcbits.com/img/a2965037780_10.jpg',
+              },
+            },
+          ];
+          this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, agentMessages),
+          }));
+        });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
+  render() {
+    return (
+      <KeyboardAvoidingView
+        style={{flex: 1, width: '100%', height: '100%'}}
+        behavior="padding"
+        enabled>
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: Constants.installationId,
+          }}
         />
-        <View style={styles.messagesContainer}>
-          <View style={[styles.messageContainer, styles.userMessageContainer]}>
-            <Text style={[styles.message, styles.userMessage]}>
-              {userMessage}
-            </Text>
-          </View>
-          <View style={[styles.messageContainer, styles.agentMessageContainer]}>
-            <Text style={[styles.message, styles.agentMessage]}>
-              {agentMessage}
-            </Text>
-          </View>
-        </View>
-        <View style={styles.form}>
-          <Input
-            style={styles.messageContainer}
-            placeholder="Send a message..."
-            ref={input => {
-              this.textInput = input;
-            }}
-            onSubmitEditing={event => {
-              queryAgent(event.nativeEvent.text);
-              this.textInput.clear();
-            }}
-          />
-        </View>
       </KeyboardAvoidingView>
-    </ThemeProvider>
-  );
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    //justifyContent: 'center',
-    alignItems: 'center',
-    height: '100%',
-    width: '100%',
-  },
-  messagesContainer: {
-    width: '100%',
-    padding: 20,
-  },
-  messageContainer: {
-    marginVertical: 20,
-    padding: 5,
-  },
-  userMessageContainer: {
-    borderColor: 'blue',
-    borderBottomWidth: 1,
-    alignItems: 'flex-end',
-    alignSelf: 'flex-end',
-    width: '80%',
-  },
-  agentMessageContainer: {
-    borderColor: 'red',
-    borderBottomWidth: 1,
-    width: '80%',
-  },
-  userMessage: {
-    textAlign: 'right',
-  },
-  agentMessage: {
-    textAlign: 'left',
-  },
-  message: {
-    fontSize: 16,
-  },
-  form: {
-    justifyContent: 'center',
-    width: '100%',
-    padding: 20,
-    marginTop: 20,
-  },
-});
+export default App;
